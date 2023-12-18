f69fc16b6156442be0e4852f042ea71ed375cc58
BIGBOWL <75559725+bigbowl99@users.noreply.github.com>
2023-12-18 13:03:07 +0800

    Update _worker.js
    using a uuid to verify identity.

--- _worker.js
+++ _worker.js
@@ -1,13 +1,29 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    url.host = "api.openai.com";
-    // openai is already set all CORS heasders 
-    return fetch(url, {
-      headers: request.headers,
-      method: request.method,
-      body: request.body,
-      redirect: 'follow'
-    });
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+// 验证 UUID
+const VALID_UUID = 'ce37b8b5-ffac-47b2-a5df-d011d3b7df7e';
+
+async function handleRequest(request) {
+  // 获取请求头中的 UUID
+  const uuid = request.headers.get('X-UUID');
+
+  // 验证 UUID
+  if (uuid !== VALID_UUID) {
+    return new Response('Unauthorized', { status: 401 });
   }
+
+  // 代理目标 URL
+  const url = 'https://chat.openai.com';
+
+  // 创建新请求，继承原始请求的一些属性
+  const newRequest = new Request(url, {
+    body: request.body,
+    headers: request.headers,
+    method: request.method,
+  });
+
+  // 发送请求到目标 URL，并返回其响应
+  return fetch(newRequest);
 }
